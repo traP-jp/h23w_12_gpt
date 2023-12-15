6a29e7e6b926569302cbcfa924167907ac651eb3
aketami <136186146+Aketami23@users.noreply.github.com>
2023-12-16 01:36:27 +0900

    Update main.py
    keyの名前の調整とstrの不良を解除

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import requests
 def generate_recipe_and_image(client, cooking_name):
     PROMPT = """
     ###見本
-        "cooking_name": "レオナルド・ダ・ウィンチ",
-        "comment": ["この名前から連想されるのは、芸術性とイタリアの伝統的な味わいです。そこで、イタリアンスタイルの豪華な料理を想像しましょう。この料理は、鮮やかな色合いとモダンな味わいが特徴です。見た目にも華やかで、特別な日にぴったりの一品になるでしょう。"],
-        "material": [
+        "name": "レオナルド・ダ・ウィンチ",
+        "comment": "この名前から連想されるのは、芸術性とイタリアの伝統的な味わいです。そこで、イタリアンスタイルの豪華な料理を想像しましょう。この料理は、鮮やかな色合いとモダンな味わいが特徴です。見た目にも華やかで、特別な日にぴったりの一品になるでしょう。",
+        "ingredient": [
             "ニンニク 2片（みじん切り）",
             "新鮮なバジル 10枚",
             "プロシュート 100g（薄切り）",
@@ -23,7 +23,7 @@ def generate_recipe_and_image(client, cooking_name):
             "黒こしょう 適量",
             "パルメザンチーズ 適量（削り）"
         ],
-        "way_of_making": [
+        "instruction": [
             "パスタを塩を入れたお湯でアルデンテに茹でる。",
             "フライパンにオリーブオイルを熱し、ニンニクを炒める。",
             "プロシュートを加えてさっと炒め、ミニトマトも加えて炒める。",
@@ -32,20 +32,20 @@ def generate_recipe_and_image(client, cooking_name):
             "モッツァレラチーズとバジルを加え、塩と黒こしょうで味を調える。",
             "皿に盛り付け、パルメザンチーズを上から削って完成。"
         ]
-        "Detailed Description": [
+        "detailed_description": [
             "A dish inspired by Leonardo da Vinci, featuring a plate of penne pasta mixed with sautéed prosciutto, mini tomatoes, and fresh basil. Topped with slices of mozzarella cheese and a sprinkle of Parmesan, garnished elegantly on a ceramic plate. The setting is artistic and sophisticated, embodying the essence of Italian cuisine with a modern twist."
             ]
         
-    "cooking_name": " ",
-    "comment": [その料理の由来,雰囲気],
-    "material": [ ],
+    "name": " ",
+    "comment": "その料理の由来,雰囲気",
+    "ingredient": [ ],
     "seasoning": [ ],
-    "way_of_making": [ ]
-    "Detailed Description": [Only this part should be written in English.
+    "instruction": [ ]
+    "detailed_description": ["Only this part should be written in English.
 1.Identify Detailed Visual Elements: I think of specific visual elements that will make the generated image concrete and original. For example, in the case of a dish, I imagine the ingredients, colors, shapes, and style of presentation in detail.
 2.Setting the Scene: The background or context of the image is also important. I create an overall image of the scene, like what kind of plate the dish is on, what the surrounding atmosphere is like, etc.
 3.Artistic and Creative Expression: To enhance the uniqueness and artistry of the image, I sometimes incorporate creative elements or distinctive styles.
-4.Color Palette: Mentioning specific colors (like deep blues, purples, and shimmering silvers) could help convey the cosmic theme more vividly.]
+4.Color Palette: Mentioning specific colors (like deep blues, purples, and shimmering silvers) could help convey the cosmic theme more vividly."]
     ###見本を参考にして与えた料理二人前のレシピを書いてjson形式で出力して
     {text}
     """.strip()
@@ -122,11 +122,11 @@ def update_json_with_image_url(file_name, image_url):
     try:
         with open(file_name, 'r+', encoding='utf-8') as file:
             data = json.load(file)
-            data["Image URL"] = image_url
+            data["picture_url"] = image_url
             file.seek(0)
             json.dump(data, file, ensure_ascii=False, indent=4)
             file.truncate()
-        print(f"Image URL added successfully to {file_name}")
+        print(f"picture_url added successfully to {file_name}")
     except Exception as e:
         print(f"An error occurred while updating the file: {e}")
 
@@ -174,8 +174,8 @@ def convert_json_to_dict(json_data):
 
 def update_dict_with_image_url(data, image_url):
     try:
-        data["Image URL"] = image_url
-        print("Image URL added successfully to dict.")
+        data["picture_url"] = image_url
+        print("picture_url added successfully to dict.")
     except Exception as e:
         print(f"An error occurred while updating the dict: {e}")
 
