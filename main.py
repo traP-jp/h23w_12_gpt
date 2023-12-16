3edaff6d95d7130f7c191c46725829a5b8ba3668
aketami <136186146+Aketami23@users.noreply.github.com>
2023-12-17 05:07:14 +0900

    Add files via upload
    完全版

--- main.py
+++ main.py
@@ -1,182 +1,190 @@
 from openai import OpenAI
-import time
 import re
-import json
 import requests
+import json
 
+# Parameters
 
-
-def generate_recipe_and_image(client, cooking_name):
-    PROMPT = """
-    ###見本
-        "cooking_name": "レオナルド・ダ・ウィンチ",
-        "comment": "この名前から連想されるのは、芸術性とイタリアの伝統的な味わいです。そこで、イタリアンスタイルの豪華な料理を想像しましょう。この料理は、鮮やかな色合いとモダンな味わいが特徴です。見た目にも華やかで、特別な日にぴったりの一品になるでしょう。",
-        "ingredient": [
-            "ニンニク 2片（みじん切り）",
-            "新鮮なバジル 10枚",
-            "プロシュート 100g（薄切り）",
-            "モッツァレラチーズ 150g（スライス）",
-            "ミニトマト 200g（半分に切る）",
-            "ペンネパスタ 200g"]
-        "seasoning":[
-            "オリーブオイル 大さじ2",
-            "白ワイン 100ml",
-            "塩 適量",
-            "黒こしょう 適量",
-            "パルメザンチーズ 適量（削り）"
-        ],
-        "instruction": [
-            "パスタを塩を入れたお湯でアルデンテに茹でる。",
-            "フライパンにオリーブオイルを熱し、ニンニクを炒める。",
-            "プロシュートを加えてさっと炒め、ミニトマトも加えて炒める。",
-            "白ワインを注ぎ、少し煮詰める。",
-            "茹で上がったパスタをフライパンに加え、全体をよく混ぜる。",
-            "モッツァレラチーズとバジルを加え、塩と黒こしょうで味を調える。",
-            "皿に盛り付け、パルメザンチーズを上から削って完成。"
+# 関数の外に書きたかったのでf-stringにしませんでした
+PROMPT = """
+###見本
+    "cooking_name": "レオナルド・ダ・ウィンチ",
+    "comment": "この名前から連想されるのは、芸術性とイタリアの伝統的な味わいです。そこで、イタリアンスタイルの豪華な料理を想像しましょう。この料理は、鮮やかな色合いとモダンな味わいが特徴です。見た目にも華やかで、特別な日にぴったりの一品になるでしょう。",
+    "ingredient": [
+        "ニンニク 2片（みじん切り）",
+        "新鮮なバジル 10枚",
+        "プロシュート 100g（薄切り）",
+        "モッツァレラチーズ 150g（スライス）",
+        "ミニトマト 200g（半分に切る）",
+        "ペンネパスタ 200g"]
+    "seasoning":[
+        "オリーブオイル 大さじ2",
+        "白ワイン 100ml",
+        "塩 適量",
+        "黒こしょう 適量",
+        "パルメザンチーズ 適量（削り）"
+    ],
+    "instruction": [
+        "パスタを塩を入れたお湯でアルデンテに茹でる。",
+        "フライパンにオリーブオイルを熱し、ニンニクを炒める。",
+        "プロシュートを加えてさっと炒め、ミニトマトも加えて炒める。",
+        "白ワインを注ぎ、少し煮詰める。",
+        "茹で上がったパスタをフライパンに加え、全体をよく混ぜる。",
+        "モッツァレラチーズとバジルを加え、塩と黒こしょうで味を調える。",
+        "皿に盛り付け、パルメザンチーズを上から削って完成。"
+    ]
+    "detailed_description": [
+        "A dish inspired by Leonardo da Vinci, featuring a plate of penne pasta mixed with sautéed prosciutto, mini tomatoes, and fresh basil. Topped with slices of mozzarella cheese and a sprinkle of Parmesan, garnished elegantly on a ceramic plate. The setting is artistic and sophisticated, embodying the essence of Italian cuisine with a modern twist."
         ]
-        "detailed_description": [
-            "A dish inspired by Leonardo da Vinci, featuring a plate of penne pasta mixed with sautéed prosciutto, mini tomatoes, and fresh basil. Topped with slices of mozzarella cheese and a sprinkle of Parmesan, garnished elegantly on a ceramic plate. The setting is artistic and sophisticated, embodying the essence of Italian cuisine with a modern twist."
-            ]
-        
-    "cooking_name": "",
-    "comment": "その料理の由来,雰囲気",
-    "ingredient": [ ],
-    "seasoning": [ ],
-    "instruction": [ ]
-    "detailed_description": ["Only this part should be written in English.
+
+"cooking_name": "{text}",
+"comment": "その料理の由来,雰囲気",
+"ingredient": [ ],
+"seasoning": [ ],
+"instruction": [ ]
+"detailed_description": ["You must  write this part in English.
 1.Identify Detailed Visual Elements: I think of specific visual elements that will make the generated image concrete and original. For example, in the case of a dish, I imagine the ingredients, colors, shapes, and style of presentation in detail.
 2.Setting the Scene: The background or context of the image is also important. I create an overall image of the scene, like what kind of plate the dish is on, what the surrounding atmosphere is like, etc.
 3.Artistic and Creative Expression: To enhance the uniqueness and artistry of the image, I sometimes incorporate creative elements or distinctive styles.
 4.Color Palette: Mentioning specific colors (like deep blues, purples, and shimmering silvers) could help convey the cosmic theme more vividly."]
-    ###見本を参考にして与えた料理二人前のレシピを書いてjson形式で出力して
-    {text}
-    """.strip()
+###見本を参考にして与えた料理二人前のレシピを書いてjson形式で出力して
+{text}
+"""
 
-    MAX_RETRIES = 3
+def query_gpt(client, prompt):
+    """
+    Send the given prompt to the GPT api to get a completion.
 
-    def query_openai(prompt):
-        retries = 0
-        while retries < MAX_RETRIES:###現在は機能していない
-            try:
-                print("\nTHINKING...")
-                response = client.chat.completions.create(
-                    model="gpt-4-1106-preview",###gpt-4-1106-previewgpt-3.5-turbo-1106
-                    messages=[{"role": "user", "content": prompt}]
-                )
-                return response
-            #except OpenAI.error.RateLimitError:
-                #print(f"RateLimitError encountered. Retrying in {RETRY_DELAY} seconds...")
-                #time.sleep(RETRY_DELAY)
-                #RETRY_DELAY *= 2
-                #retries += 1
-            except Exception as e:
-                print(e)
-            break
-        else:
-            print("Failed to get a response after multiple retries.")
-            return None
+    Parameters
+    ----------
+    client: ?
+        OpenAI client
+    prompt: str
+        prompt to generate a recipe
 
-    formatted_prompt = PROMPT.format(text=cooking_name)###プロンプトに入力（料理名）を埋め込み
-    response = query_openai(formatted_prompt)###埋め込んだプロンプトをopenaiに投げる
-    
-    print(response)
+    Returns
+    -------
+    recipe_info: dict
+        success request: dict that contains information about the recipe other than the image
+        otherwise: 0
+    image_prompt: str
+        success request: prompt to generate image
+        otherwise: 0
+    """
+    GPT_prompt = PROMPT.format(text=prompt) # プロンプトに入力（料理名）を埋め込み
 
-    ###detailed_descriptionの出力を整形してjsonにしてからDALLにプロンプトとして渡す
-    if response:
+    try:
+        # GPTモデルに問い合わせ
+        response = client.chat.completions.create(
+            model='gpt-3.5-turbo-1106',  # gpt-4-1106-preview or gpt-3.5-turbo-1106でモデルを選択
+            messages=[{"role": "user", "content": GPT_prompt}]
+        )
+        if not response:
+            return 0, 0
+
+        # JSON形式のテキストを抽出，直接dictにできませんでした
         json_part = re.search(r'\{.*\}', response.choices[0].message.content, re.DOTALL)
-        extracted_json = json_part.group() if json_part else "No JSON format found"
+        extracted_json = json_part.group()
+        recipe_info = json.loads(extracted_json)
 
+        # 画像生成用のプロンプトを抽出
         match = re.search(r'"detailed_description": \[([^\]]*)\]', response.choices[0].message.content)
         if match:
-            detailed_description = match.group(1).strip().strip('"')
+            image_prompt = match.group(1).strip().strip('"')
         else:
-            detailed_description = "No detailed_description found."
+            return 0, 0
 
-        image_response = client.images.generate(
-            model="dall-e-3",
-            prompt="Draw a complete dish on one plate.{}".format(detailed_description),
-            size="1024x1024",
-            quality="standard",
-            n=1,
-            style="vivid"
+    except Exception:
+        return 0, 0
+    return recipe_info, image_prompt
+
+
+def query_dall_e(client, prompt):
+    """
+    Send the given prompt to dall-e-3 api to get an image.
+
+    Parameters
+    ----------
+    client: ?
+        OpenAI client
+    prompt: str
+        prompt for generating image generated by query_gpt
+
+    Returns
+    -------
+    image_url: str
+        url to retrieve the image generated by dall-e-3
+    """
+    try:
+        # DALL-Eモデルに問い合わせ
+        response = client.images.generate(
+            model = 'dall-e-3',
+            prompt = f'Draw a complete dish on one plate.{prompt}',
+            size = '1024x1024',
+            n = 1,
+            style = 'vivid'
         )
-        
-        image_url = image_response.data[0].url if image_response else "No image generated"###urlのみを選ぶためのところ
-        return extracted_json, image_url###一つ目の関数の出力となる
-    else:
-        return "Failed to generate recipe", "No image generated"
+
+        # 生成された画像のURLを取得
+        ig_url = response.data[0].url
+        # ToDo: I have to check that it is enough the unexpecteed error.
+        return  ig_url
+    except:
+        return 0, 0
 
 
-def save_json_to_file(json_data, file_name):##jsonファイルを作って保存
+def get_image_from_url(url):
+    """
+    Downloads an image from the specified URL and converts it to binary data.
+
+    Parameters
+    ----------
+    url : str
+        The URL of the image to download.
+
+    Returns
+    -------
+    binary_image : bytes
+        The binary data of the image.
+    """
     try:
-        data = json.loads(json_data)
-        with open(file_name, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-        print(f"File saved successfully as {file_name}")
-        return file_name
-    except json.JSONDecodeError:
-        print("Failed to decode JSON data.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-def update_json_with_image_url(file_name, image_url):##josnファイルにurlをかきこみ
-    try:
-        with open(file_name, 'r+', encoding='utf-8') as file:
-            data = json.load(file)
-            data["picture_url"] = image_url
-            file.seek(0)
-            json.dump(data, file, ensure_ascii=False, indent=4)
-            file.truncate()
-        print(f"picture_url added successfully to {file_name}")
-    except Exception as e:
-        print(f"An error occurred while updating the file: {e}")
-
-def download_and_save_image(image_url, file_name):###url先の画像を保存する
-    try:
-        response = requests.get(image_url)
+        # URLから画像をダウンロード
+        response = requests.get(url)
         response.raise_for_status()
 
-        base_file_name = file_name.rsplit('.', 1)[0]
+        # 画像をバイナリデータとして取得
+        binary_image = response.content
+        return binary_image
+        
+    except:
+        return 0, 0
 
-        image_file_name = f"{base_file_name}.jpg"
 
-        with open(image_file_name, 'wb') as file:
-            file.write(response.content)
+def executor(name):
+    """
+    Generates a recipe and image from a given dish name and returns them in dict.
 
-        print(f"Image saved successfully as {image_file_name}")
-    except requests.RequestException as e:
-        print(f"An error occurred while downloading the image: {e}")
+    Parameters
+    ----------
+    name: str
 
-def convert_json_to_dict(json_data):###jsonをdictにして返す
-    try:
-        data = json.loads(json_data)
-        print("JSON data successfully converted to dict.")
-        return data
-    except json.JSONDecodeError:
-        print("Failed to decode JSON data.")
-        return None
+    Returns
+    -------
+    recipe_info: dict
+        dict with cooking_name, comment, ingredient, seasoning, instruction, and image as keys
+    """
+    client = OpenAI()
 
-def update_dict_with_image_url(data, image_url):###dcitにurlを書き込み
-    try:
-        data["picture_url"] = image_url
-        print("picture_url added successfully to dict.")
-    except Exception as e:
-        print(f"An error occurred while updating the dict: {e}")
+    # GPTモデルを用いてレシピ情報と画像生成用プロンプトを取得
+    recipe_info, image_prompt = query_gpt(client, name)
 
-# 使用部分
-client = OpenAI()###apikyeの設定（環境変数を使っていれば空欄）
-recipe_name = "地中グミ"###ここに料理名
-recipe_json, image_url = generate_recipe_and_image(client, recipe_name)
+    # DALL-Eモデルを用いて画像を生成
+    image_url = query_dall_e(client, image_prompt)
 
-# JSONデータの検証と辞書への変換
-recipe_dict = convert_json_to_dict(recipe_json)
-if recipe_dict is None:
-    print("Received JSON data is not valid.")
-else:
-    saved_file_name = save_json_to_file(recipe_json, "recipe14.json")
-    
-    if saved_file_name is not None:
-        download_and_save_image(image_url, saved_file_name)
+    # 画像のURLからバイナリデータを取得
+    recipe_info['image'] = get_image_from_url(image_url)
 
-    update_dict_with_image_url(recipe_dict, image_url)
-###recipe_dictが欲しかったdictの料理データ
+    return recipe_info
+
+recipe = executor("秋葉荊棘")
+print(recipe)
